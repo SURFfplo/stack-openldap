094c68eb22ebd1c1dba1ad297c95329ebb19a614
Herman <herman@dompseler.nl>
2019-06-13 15:50:52 +0200

    prepare for multiple deployments

--- go.sh
+++ go.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-export STACK_NETWORK=dev-net
-export STACK_SERVICE=openldap
-export STACK_VERSION=0.1
+# defaults
+SERVICE=openldap
+VERSION=0.1
+NETWORK=dev-net
+PORT=389
+
+# input with four arguments: go.sh SERVICE VERSION NETWORK PORT
+if [ "$1" != "" ]; then
+        SERVICE=$1
+fi
+if [ "$2" != "" ]; then
+        VERSION=$2
+fi
+if [ "$3" != "" ]; then
+        NETWORK=$3
+fi
+if [ "$4" != "" ]; then
+        PORT=$4
+fi
+
+# reuse input
+export STACK_SERVICE=$SERVICE
+export STACK_VERSION=$VERSION
+export STACK_NETWORK=$NETWORK
+export STACK_PORT=$PORT
 
 # prepare
 ./prepare.sh
--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # create nfs mount
-mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/conf
-mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/data
+mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE/$STACK_VERSION/conf
+mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE/$STACK_VERSION/data
 
